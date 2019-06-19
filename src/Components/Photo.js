755e56ec0b0ba57018965ec79d8635c3e4316f36
Jasper Kim <yjikim0219@gmail.com>
2019-06-19 15:49:16 -0700

    Update comment

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Photo = (props) => {
+    //Add variables to store the data through props from PhotoList component
     const farm = props.farm;
     const server = props.server;
     const id = props.id;
     const secret = props.secret;
     const title = props.title
 
+    //Render li element with variables
     return (
         <li>
             <img src={`https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`} alt={title} />
--- src/Components/PhotoList.js
+++ src/Components/PhotoList.js
@@ -3,15 +3,20 @@ import Photo from './Photo';
 import NoPhoto from './NoPhoto';
 
 const PhotoList = (props) => {
+    //Two variables, one is an array to store the data through props from App compenent.
+    //The other one is to store Photo components created by iteration of the first variable.
     const result = props.data;
     let photos;
+
+    //If the lengh of the array, which is the data from Flickr API, greater than 0, 
+    //Iterate the array, create a series of Photo components and assign it to photos variable.
+    //Otherwise, photos variable store NoPhoto component
     if(result.length > 0) {
         photos = result.map(photo => (<Photo key={photo.id} farm={photo.farm} server={photo.server} id={photo.id} secret={photo.secret} title={photo.title} />));
     } else {
         photos = <NoPhoto />;
     }
     
-
     return (
         
         <div className="photo-container">
